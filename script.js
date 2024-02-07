c6858bb4c85dc41af353de439213a14bb2b59f74
Thu Hsanda <131221167+thuhsanda@users.noreply.github.com>
2024-02-07 12:32:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function generateMessage(noCount) {
 }
 
 function changeImage(image) {
-  catImg.src = `cat-${image}.jpg`;
+  catImg.src = `img/cat-${image}.jpg`;
 }
 
 function updateNoButtonText() {
